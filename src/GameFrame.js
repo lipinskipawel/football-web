26a334324f81ca139f58c913f05dbf9271bb6961
Pawel Lipinski <lipinskijob@gmail.com>
2021-01-31 20:16:19 +0100

    Take the canvas context ref only once

--- src/GameFrame.js
+++ src/GameFrame.js
@@ -1,16 +1,23 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const width = 500;
 const height = 600;
 
 const GameFrame = () => {
   const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    contextRef.current = context;
+  }, []);
+
   const handleOnClick = (event) => {
+    console.log(contextRef);
     event.stopPropagation();
     const x = event.nativeEvent.offsetX;
     const y = event.nativeEvent.offsetY;
-    const cxt = canvasRef.current.getContext("2d");
-    cxt.fillRect(x, y, 20, 20);
+    contextRef.current.fillRect(x, y, 20, 20);
   };
 
   return (
