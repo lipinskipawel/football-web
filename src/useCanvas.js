98c9b3092a43c9fece363c6bf1a504d3b70c294d
Pawel Lipinski <lipinskijob@gmail.com>
2021-02-11 20:05:14 +0100

    Draw gaming points on canvas

--- src/useCanvas.js
+++ src/useCanvas.js
@@ -1,5 +1,28 @@
 import { useEffect, useRef } from "react";
 
+function drawDots(width, height, cxt) {
+  console.log("width: " + width);
+  console.log("height: " + height);
+  const spaceBetweenDotsH = width / 9;
+  const shiftDotsHorizontally = spaceBetweenDotsH / 2;
+
+  const spaceBetweenDotsV = height / 13;
+  const shiftDotsVertically = spaceBetweenDotsV / 2;
+  const dotMargin = 0;
+  const dotSize = 5;
+
+  cxt.strokeStyle = "white";
+  cxt.fillStyle = "white";
+  for (var i = shiftDotsHorizontally; i < width; i += spaceBetweenDotsH) {
+    for (var j = shiftDotsVertically; j < height; j += spaceBetweenDotsV) {
+      cxt.beginPath();
+      cxt.arc(i - dotMargin, j - dotMargin, dotSize, 0, Math.PI * 2, false);
+      cxt.fill();
+      cxt.stroke();
+    }
+  }
+}
+
 const useCanvas = () => {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
@@ -7,10 +30,11 @@ const useCanvas = () => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
     contextRef.current = context;
-    contextRef.current.beginPath();
     // context.fillStyle = "green";
     context.fillStyle = "rgba(0, 170, 45)";
     context.fillRect(0, 0, canvas.width, canvas.height);
+    drawDots(canvas.width, canvas.height, contextRef.current);
+    contextRef.current.beginPath();
   }, []);
 
   const handleOnClick = ({ nativeEvent }) => {
