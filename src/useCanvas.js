1dd3617f46f955d1458ff86ef6cf5c35c9b9d411
Pawel Lipinski <lipinskijob@gmail.com>
2021-02-12 20:21:12 +0100

    Delete console.log and commented code

--- src/useCanvas.js
+++ src/useCanvas.js
@@ -1,14 +1,11 @@
 import { useEffect, useRef } from "react";
 
 function drawDots(width, height, cxt) {
-  console.log("width: " + width);
-  console.log("height: " + height);
   const spaceBetweenDotsH = width / 9;
   const shiftDotsHorizontally = spaceBetweenDotsH / 2;
 
   const spaceBetweenDotsV = height / 13;
   const shiftDotsVertically = spaceBetweenDotsV / 2;
-  const dotMargin = 0;
   const dotSize = 5;
 
   cxt.strokeStyle = "white";
@@ -16,7 +13,7 @@ function drawDots(width, height, cxt) {
   for (var i = shiftDotsHorizontally; i < width; i += spaceBetweenDotsH) {
     for (var j = shiftDotsVertically; j < height; j += spaceBetweenDotsV) {
       cxt.beginPath();
-      cxt.arc(i - dotMargin, j - dotMargin, dotSize, 0, Math.PI * 2, false);
+      cxt.arc(i, j, dotSize, 0, Math.PI * 2, false);
       cxt.fill();
       cxt.stroke();
     }
@@ -80,7 +77,6 @@ const useCanvas = () => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
     contextRef.current = context;
-    // context.fillStyle = "green";
     context.fillStyle = "rgba(0, 170, 45)";
     context.fillRect(0, 0, canvas.width, canvas.height);
     drawDots(canvas.width, canvas.height, contextRef.current);
