db4b57c6495dceba6e81efdd9e7204ec26f565b7
Pawel Lipinski <lipinskijob@gmail.com>
2021-02-15 21:55:52 +0100

    Add test for GameEngine compute dots

--- src/GameEngine.test.js
+++ src/GameEngine.test.js
@@ -1,5 +1,19 @@
 import GameEngine from "./GameEngine";
 
+test("should compute staring points and space between points correctly", () => {
+  const dots = GameEngine.computeDots(90, 130);
+
+  const firstX = dots.startingH;
+  const firstY = dots.startingV;
+  const spaceBetweenDotsH = dots.spaceBetweenDotsH;
+  const spaceBetweenDotsV = dots.spaceBetweenDotsV;
+
+  expect(firstX).toEqual(5);
+  expect(firstY).toEqual(5);
+  expect(spaceBetweenDotsH).toEqual(10);
+  expect(spaceBetweenDotsV).toEqual(10);
+});
+
 test("should allow to make a move", () => {
   const game = new GameEngine();
 
