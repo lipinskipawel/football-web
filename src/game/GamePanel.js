701b05dc7ad843796eebfa85166ff2799dbc63d5
Pawel Lipinski <lipinskijob@gmail.com>
2021-09-03 21:00:55 +0200

    Handle any errors related to websocket connection

--- src/game/GamePanel.js
+++ src/game/GamePanel.js
@@ -3,12 +3,17 @@ import useWebSocket from "../useWebSocket";
 import config from "../config";
 
 const GamePanel = () => {
-  const { send } = useWebSocket(config.webSocket.serverUrl);
+  const { send, error } = useWebSocket(config.webSocket.serverUrl);
 
   return (
     <>
-      <div>GamePanel</div>
-      <GameFrame onMove={(data) => send(JSON.stringify(data))} />
+      {error && <div>Error</div>}
+      {!error && (
+        <>
+          <div>GamePanel</div>
+          <GameFrame onMove={(data) => send(JSON.stringify(data))} />
+        </>
+      )}
     </>
   );
 };
--- src/useWebSocket.js
+++ src/useWebSocket.js
@@ -2,32 +2,43 @@ import { useEffect, useState } from "react";
 
 const useWebSocket = (url) => {
   const [socket, setSocket] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const socket = new WebSocket(url);
     socket.onopen = () => {
       console.log("onopen");
       setSocket(socket);
+      setError(null);
     };
     socket.onmessage = (event) => {
       console.log("onmessage");
       console.log(event);
       console.log(event.data);
     };
+    socket.onerror = () => {
+      setSocket(null);
+      setError(true);
+    };
 
     return () => {
       if (socket != null) {
         socket.close();
         setSocket(null);
+        setError(null);
       }
     };
   }, [url]);
 
   const send = (dataToSend) => {
-    socket.send(dataToSend);
+    if (socket != null) {
+      socket.send(dataToSend);
+    } else {
+      setError(true);
+    }
   };
 
-  return { send };
+  return { send, error };
 };
 
 export default useWebSocket;
